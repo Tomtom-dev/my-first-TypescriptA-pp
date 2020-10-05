e92a167ab3e3954d2c6816a86a2d5c2d7acd867a
Tomtom-dev <thomasheddadji0@gmail.com>
2020-10-05 17:35:39 +0200

    add some inferface

--- index.ts
+++ index.ts
@@ -2,8 +2,23 @@ import axios from "axios"
 
 const url= "https://jsonplaceholder.typicode.com/todos/2"
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
 axios.get(url).then(response =>{
-    console.log(response.data);
+
+    const todo= response.data as Todo
     
+    const id= todo.id;
+    const title= todo.title;
+    const completed= todo.completed;
+
+    console.log(`
+    the todo with ID: ${id}
+    had a title of: ${title}
+    is it finished? : ${completed}
+    `); 
 })
